e0668e7cb90e1e3a09d02556fad74bdaa7526a39
Naim <naim.bouallagui.8@gmail.com>
2020-03-18 18:31:50 +0100

    fix getBookById api

--- Apis/bookApis.js
+++ Apis/bookApis.js
@@ -7,24 +7,18 @@ const BookModel = require("../Models/bookSchema").BookModel;
  */
 router.get("/getAllBooks", (req, res) => {
   BookModel.find((err, result) => {
-    if (err) {
-      res.status(500).send(err);
-    } else {
-      res.send(result);
-    }
+    if (err) res.status(500).send(err);
+    else res.send(result);
   });
 });
 
 /**
  *  getBookById
  */
-router.get("/getBook/:id", async (req, res) => {
-  await BookModel.findById(req.params.id, (err, result) => {
-    if (err) {
-      res.status(500).send(err);
-    } else {
-      res.send(result);
-    }
+router.get("/getBook/:id", (req, res) => {
+  BookModel.findById(req.params.id, (err, result) => {
+    if (err) res.status(500).send(err);
+    else res.send(result);
   });
 });
 
