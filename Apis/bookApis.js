308e989ad8749bfded088e9fbe6108c9c42560d4
Naim <naim.bouallagui.8@gmail.com>
2020-03-18 15:15:18 +0100

    add getBookById api

--- Apis/bookApis.js
+++ Apis/bookApis.js
@@ -2,11 +2,24 @@ const express = require('express');
 const router = express.Router();
 const BookModel = require('../Models/bookSchema').BookModel;
 
-// =========
-// Read many
-// =========
+/**
+ * getAllBooks
+ */
 router.get('/getAllBooks', (req, res) => {
   BookModel.find((err, result) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.send(result);
+    }
+  });
+});
+
+/**
+ *  getBookById
+ */
+router.get('/getBook/:id', async (req, res) => {
+  await BookModel.findById(req.params.id, (err, result) => {
     if (err) {
       res.status(500).send(err);
     } else {
